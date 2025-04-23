8e318791b8e38f6622db9bd6f3877b61a6dcd2ad
Eddy dashner <eddy.nzoabarinda@amalitech.com>
2025-04-23 15:09:33 +0200

    constructor injection

---
+++ src/main/java/com/eddyspringboot/store/StripePaymentService.java
@@ -0,0 +1,9 @@
+package com.eddyspringboot.store;
+
+public class StripePaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount){
+        System.out.println("STRIPE");
+        System.out.println("Amount:" + amount);
+    }
+}
